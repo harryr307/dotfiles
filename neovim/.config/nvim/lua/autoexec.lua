ad9582d13c80dddf9100108d48f1f966c36bf9e0
Harry Roberts <harryroberts306@gmail.com>
2026-04-02 18:16:18 -0400

    Completely modularise Neovim

---
+++ neovim/.config/nvim/lua/autoexec.lua
@@ -0,0 +1,12 @@
+-- Enable transparent background
+vim.cmd([[
+   highlight Normal ctermbg=NONE guibg=NONE
+   highlight NonText ctermbg=NONE guibg=NONE
+   highlight NormalNC ctermbg=none guibg=none
+   highlight VertSplit ctermbg=none guibg=none
+   highlight MsgArea ctermbg=none guibg=none
+   highlight FloatBorder ctermbg=none guibg=none
+   highlight FloatTitle ctermbg=none guibg=none
+   highlight Pmenu ctermbg=none guibg=none
+   highlight PmenuSel ctermbg=none guibg=none
+]])
---
+++ neovim/.config/nvim/lua/custom/plugins/neo-tree.lua
@@ -0,0 +1,23 @@
+return { -- Neotree for file tree
+  "nvim-neo-tree/neo-tree.nvim",
+  branch = "v3.x",
+  opts = {
+    window = {
+      mappings = {
+        ["<CR>"] = "open_tabnew",
+        ["<2-LeftMouse>"] = "open_tabnew",
+        ["t"] = "open_tabnew",
+        ["<C-c>"] = "open",
+        ["s"] = "open_split",
+        ["v"] = "open_vsplit",
+      },
+    },
+  },
+  dependencies = {
+    "nvim-lua/plenary.nvim",
+    "MunifTanjim/nui.nvim",
+    "3rd/image.nvim",
+    "nvim-tree/nvim-web-devicons",
+  },
+  lazy = false,
+}
